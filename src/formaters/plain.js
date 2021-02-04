f8634697fa0ecedcd69da6e6885aaf17026348b6
Kid-Guru <birkov.alexandr@gmail.com>
2021-02-04 14:42:00 +0400

    fix without sort()

--- src/formaters/plain.js
+++ src/formaters/plain.js
@@ -2,19 +2,19 @@ import isObject from 'lodash/isObject.js';
 import isArray from 'lodash/isArray.js';
 import isString from 'lodash/isString.js';
 
-const outputTemplates = {
-  added: ({ name, value }) => `Property '${name.join('.')}' was added with value: ${value}`,
-  deleted: ({ name }) => `Property '${name.join('.')}' was removed`,
-  changed: ({ name, valueOld, value }) => `Property '${name.join('.')}' was updated. From ${valueOld} to ${value}`,
-};
-const checkStatus = (status) => {
-  if (status === 'added'
-    || status === 'deleted'
-    || status === 'changed') {
-    return true;
-  }
-  return false;
-};
+// const outputTemplates = {
+//   added: ({ name, value }) => `Property '${name.join('.')}' was added with value: ${value}`,
+//   deleted: ({ name }) => `Property '${name.join('.')}' was removed`,
+//   changed: ({ name, valueOld, value }) => `Property '${name.join('.')}' was updated. From ${valueOld} to ${value}`,
+// };
+// const checkStatus = (status) => {
+//   if (status === 'added'
+//     || status === 'deleted'
+//     || status === 'changed') {
+//     return true;
+//   }
+//   return false;
+// };
 const prettyValue = (value) => {
   if (isObject(value) || isArray(value)) return '[complex value]';
   if (isString(value)) return `'${value}'`;
@@ -22,24 +22,26 @@ const prettyValue = (value) => {
 };
 
 const plain = (ast) => {
-  const plainOutput = [];
-  const iter = (tree, name) => tree.forEach((node) => {
+  // const plainOutput = [];
+  const iter = (tree, name) => tree.flatMap((node) => {
     const nestedName = [...name, node.name];
-    if (checkStatus(node.status) === true) {
-      const template = outputTemplates[node.status];
-      const value = prettyValue(node.value);
-      const valueOld = prettyValue(node.valueOld);
-      const render = template({
-        ...node, name: nestedName, value, valueOld,
-      });
-      plainOutput.push(render);
-    }
-    if (node.children !== false) {
-      iter(node.children, nestedName);
+    const value = prettyValue(node.value);
+    const valueOld = prettyValue(node.valueOld);
+    switch (node.status) {
+      case ('added'):
+        return `Property '${nestedName.join('.')}' was added with value: ${value}`;
+      case ('deleted'):
+        return `Property '${nestedName.join('.')}' was removed`;
+      case ('changed'):
+        return `Property '${nestedName.join('.')}' was updated. From ${valueOld} to ${value}`;
+      case ('nested'):
+        return iter(node.children, nestedName);
+      case ('unchanged'):
+      default:
+        return [];
     }
   });
-  iter(ast, []);
-  return plainOutput.join('\n');
+  return iter(ast, []).join('\n');
 };
 
 export default plain;
