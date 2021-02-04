301b5cc9c3d58845c29d24ed96af61d4f92ddcf8
Kid-Guru <birkov.alexandr@gmail.com>
2021-02-04 14:45:28 +0400

    remove old code

--- src/formaters/plain.js
+++ src/formaters/plain.js
@@ -2,19 +2,6 @@ import isObject from 'lodash/isObject.js';
 import isArray from 'lodash/isArray.js';
 import isString from 'lodash/isString.js';
 
-// const outputTemplates = {
-//   added: ({ name, value }) => `Property '${name.join('.')}' was added with value: ${value}`,
-//   deleted: ({ name }) => `Property '${name.join('.')}' was removed`,
-//   changed: ({ name, valueOld, value }) => `Property '${name.join('.')}' was updated. From ${valueOld} to ${value}`,
-// };
-// const checkStatus = (status) => {
-//   if (status === 'added'
-//     || status === 'deleted'
-//     || status === 'changed') {
-//     return true;
-//   }
-//   return false;
-// };
 const prettyValue = (value) => {
   if (isObject(value) || isArray(value)) return '[complex value]';
   if (isString(value)) return `'${value}'`;
@@ -22,7 +9,6 @@ const prettyValue = (value) => {
 };
 
 const plain = (ast) => {
-  // const plainOutput = [];
   const iter = (tree, name) => tree.flatMap((node) => {
     const nestedName = [...name, node.name];
     const value = prettyValue(node.value);
